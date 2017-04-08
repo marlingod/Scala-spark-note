bc90eb7b77c435ed5c591861c249f7052bdee8ec
marlingod <gyemalin@gmail.com>
2017-04-07 21:38:27 -0500

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,2 +1,10 @@
 #Create the same schema in pyhon
-## Read the 
+from pyspark.sql.types import *
+
+# read the file 
+with open("test3.txt", "r") as myFile: 
+  contents =myFile.read().replace('\', '')
+
+fields = [StructField(fieldName, StringType, True) for fieldName in contents.split(" ")]
+schema = StructType(fields)
+                                  
