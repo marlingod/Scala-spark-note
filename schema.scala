0e142accc20a2a73c80f0475358ab934d4041719
marlingod <gyemalin@gmail.com>
2017-04-07 21:18:29 -0500

    Create schema.scala

---
+++ schema.scala
@@ -0,0 +1,19 @@
+#programmaticaly load create a dataframe in Scala
+## Create the Schema. 
+The Schema use StrucType which is nothing but Seq[StructField] . 
+Ref : https://spark.apache.org/docs/2.0.2/api/java/org/apache/spark/sql/types/StructType.html
+###load the file that contains the fieldname
+Important note use scala way of upload the file(probabibly can be done in python to be followed....)
+
+import scala.io.Source
+import org.apache.spark.sql.types._
+
+val myFile = "test.txt"
+val Contents = Source.fromFile(myFile).getLines.mkString
+
+##Create the collection of structField
+val fields = myFile.split(" ").map(fieldName => StructField(FieldName, StringType, nullable=true))
+
+# create the schema
+fields = StructType(fields)
+
